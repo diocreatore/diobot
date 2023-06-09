3ab681d6a9c2fecde63076252929dac4d7bd572d
diocreatore <102768739+diocreatore@users.noreply.github.com>
2023-06-09 13:52:09 +0200

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ import { fileURLToPath } from 'url'
 //inserisci il tuo o i tuoi numeri 
 
 global.owner = [
-  ['39xxxxxxxx', 'nobody|']
+  ['393296080130', 'nobody|']
 
 ] //inserisci il tuo o i tuoi numeri 
 
